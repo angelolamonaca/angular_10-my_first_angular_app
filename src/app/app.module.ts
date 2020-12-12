73bdc3cbf1a52fb79d99d2c681da9aa25d51bfdd
Angelo Lamonaca <angelo.lamonaca@gmail.com>
2020-12-12 11:44:36 +0100

    Vamos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NbActionsModule, NbThemeModule} from '@nebular/theme';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,10 @@ import {FormsModule} from '@angular/forms';
 import { AchievementDetailComponent } from './achievement-detail/achievement-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewDashboardComponent } from './new-dashboard/new-dashboard.component';
+import { RouterModule } from '@angular/router'; // we also need angular router for Nebular to function properly
+import { NbSidebarModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
+import {routes} from '@nebular/auth';
 
 @NgModule({
   declarations: [
@@ -20,12 +25,20 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     AchievementsComponent,
     AchievementDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    NewDashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    NbThemeModule.forRoot({name: 'cosmic'}),
+    FormsModule,
+    RouterModule.forRoot(routes, {useHash: true}),
+    NbLayoutModule,
+    NbSidebarModule.forRoot(),
+    NbButtonModule,
+    NbActionsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
